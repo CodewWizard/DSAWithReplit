3bd8b1b4285bc0be0b8437fc800e454646a4c804
Madiha Mujawar <97935861+CodewWizard@users.noreply.github.com>
2023-02-18 13:12:24 +0000

    game of 2 stacks

--- QueueUsingStacks.java
+++ QueueUsingStacks.java
@@ -3,51 +3,51 @@ import java.util.*;
 public class QueueUsingStacks {
   public static void main(String[] args) {
     Stack<Integer> stack = new Stack<>();
-    stack.push(8);
+    stack.push(10);
     System.out.println(stack.pop());
   }
 
-  class Main{
+  class Main {
     private Stack<Integer> first;
     private Stack<Integer> second;
-    
-    public Main(){
+
+    public Main() {
       first = new Stack<>();
       second = new Stack<>();
     }
 
-    public void add(int item){
+    public void add(int item) {
       first.push(item);
     }
 
-    public int remove() throws Exception{
-      while(!first.isEmpty()){
+    public int remove() throws Exception {
+      while (!first.isEmpty()) {
         second.push(first.pop());
       }
       int removed = second.pop();
 
-      while(!second.isEmpty()){
-        first.push(second.push());
+      while (!second.isEmpty()) {
+        first.push(second.pop());
       }
       return removed;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
       return first.isEmpty();
     }
 
-    public int peek() throws Exception{
-      while(!first.isEmpty()){
+    public int peek() throws Exception {
+      while (!first.isEmpty()) {
         second.push(first.pop());
       }
       int peek_val = second.peek();
 
-      while(!second.isEmpty()){
-        first.push(second.push());
+      while (!second.isEmpty()) {
+        first.push(second.pop());
       }
       return peek_val;
     }
-      
+
   }
-  
+
 }
